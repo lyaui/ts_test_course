96a3b954ce27fe6b4950196e0d4a13624a9ae7b4
Iris Huang <peiyu.huang@mile.cloud>
2024-03-08 13:23:14 +0800

    VSCode debug configuration

--- src/app/test/Utils.test.ts
+++ src/app/test/Utils.test.ts
@@ -15,7 +15,7 @@ describe('Utils test suite', () => {
   });
 
   describe('Utils test suite', () => {
-    it('Should return correct upperCase', () => {
+    it.only('Should return correct upperCase', () => {
       const actual = sut.toUpperCase('abc');
       expect(actual).toBe('ABC');
     });
