2d19c97a45949ec29fe65eccda51ed3ead1b7302
Iris Huang <lyaui@outlook.com>
2024-03-09 03:38:22 +0800

    PassChecker Iteration 1

--- src/__tests__/pass_checker/PasswordChecker.test.ts
+++ src/__tests__/pass_checker/PasswordChecker.test.ts
@@ -6,7 +6,28 @@ describe('PasswordChecker test suite', () => {
     sut = new PasswordChecker();
   });
 
-  it('Should not do anything this moment', () => {
-    sut.checkPassword();
+  it('password with less than 8 chars is invalid', () => {
+    const actual = sut.checkPassword('1234567');
+    expect(actual).toBe(false);
+  });
+
+  it('password with no upper case letter is invalid', () => {
+    const actual = sut.checkPassword('12345678abc');
+    expect(actual).toBe(false);
+  });
+
+  it('password with upper case letter is ok', () => {
+    const actual = sut.checkPassword('12345678abcA');
+    expect(actual).toBe(true);
+  });
+
+  it('password with no lower case letter is invalid', () => {
+    const actual = sut.checkPassword('12345678ABC');
+    expect(actual).toBe(false);
+  });
+
+  it('password with lower case letter is ok', () => {
+    const actual = sut.checkPassword('12345678ABCa');
+    expect(actual).toBe(true);
   });
 });
--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -1,3 +1,14 @@
+/* A password is invalid if: 
+  - length is less than 8 chars
+  - has no upper case letter 
+  - haas no lower case letter
+ */
+
 export class PasswordChecker {
-  public checkPassword() {}
+  public checkPassword(password: string): boolean {
+    if (password.length < 8) return false;
+    if (password.toLowerCase() === password) return false;
+    if (password.toUpperCase() === password) return false;
+    return true;
+  }
 }
